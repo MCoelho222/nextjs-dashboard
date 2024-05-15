b139227dfce847aaabff270c62e897b3161bf52c
Marcelo Coelho <mcoelho2011@hotmail.com>
2024-05-15 20:26:17 -0300

    feat: chapter 11 in progress - use client not working

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,8 +1,6 @@
-import { Card } from '../../ui/dashboard/cards';
 import RevenueChart from '../../ui/dashboard/revenue-chart';
 import LatestInvoices from '../../ui/dashboard/latest-invoices';
 import { lusitana } from '../../ui/fonts';
-import { fetchLatestInvoices } from '../../lib/data';
 import { Suspense } from 'react';
 import {
   RevenueChartSkeleton,
@@ -12,13 +10,6 @@ import {
 import CardWrapper from '../../ui/dashboard/cards';
 
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
-  // const {
-  //   totalPaidInvoices,
-  //   totalPendingInvoices,
-  //   numberOfInvoices,
-  //   numberOfCustomers,
-  // } = await fetchCardData();
 
   return (
     <main>
@@ -26,14 +17,6 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
